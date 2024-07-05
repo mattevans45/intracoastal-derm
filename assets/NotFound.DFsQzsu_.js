d2c3b03b1507939b3f9fd7e1098b7725116746ea
Matt Evans <A01048047@aggies.usu.edu>
2024-07-05 16:47:35 -0600

    Updates

--- assets/NotFound.DFsQzsu_.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index.D68CNNLW.js";import"./vendor.CFizi_Mu.js";const e=()=>o.jsxs("div",{children:[o.jsx("h1",{children:"404 - Not Found"}),o.jsx("p",{children:"The page you are looking for does not exist."})]});export{e as default};
